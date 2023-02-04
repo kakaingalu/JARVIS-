af1abc7365eeee84542e5bdb016be27f9deea2bd
Joseph Kakai <josephngalu96@gmail.com>
2023-02-04 09:40:15 -0800

    A searchbot

---
+++ searchbot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# This is a simple search bot module
+def search(query):
+      results = []
+        # code to search and retrieve results based on the query
+
+            return results
+
+        query = input("Enter your search query: ")
+        results = search(query)
+
+        for result in results:
+              print(result)
+search_and_print(query)              
